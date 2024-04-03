ff9e4ad7d60b399f91620447f65aca105f457ba9
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2024-04-03 16:03:30 -0400

    update text in post display

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default async function Home() {
   const postsResponse = await axios.get("https://queer-global-strapi.onrender.com/api/posts")
 
   const posts = postsResponse.data.data
-  // console.log(postsResponse.data.data[1].attributes)
+  // console.log(postsResponse.data.data[1].attributes.content[0].children)
 
 
 
@@ -23,6 +23,7 @@ export default async function Home() {
           <div key={post.attributes.publishedAt}>
             <h2>{post.attributes.title}</h2>
             <h4>{post.attributes.description}</h4>
+            <p>{post.attributes.content[1].children[0].text}</p>
           </div>
         ))
       }
