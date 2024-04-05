551555211bfbbd7b260074ad299a3b83a82b36be
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2024-04-05 18:30:37 -0400

    adds images

--- app/page.js
+++ app/page.js
@@ -3,13 +3,16 @@ import axios from "axios"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import Link from "next/link"
+import Image from "next/image"
 
 export default async function Home() {
 
-
-  const postsResponse = await axios.get("http://localhost:1337/api/posts")
+  const postsResponse = await axios.get("http://localhost:1337/api/posts?populate=*")
+  // const postsResponse = await axios.get("http://localhost:1337/api/posts")
   // const postsResponse = await axios.get("https://queer-global-strapi.onrender.com/api/posts")
   const posts = postsResponse.data.data
+
+  // console.log(posts[0].attributes.Image.data.attributes.formats.thumbnail.url)
 
 
   return (
@@ -19,9 +22,20 @@ export default async function Home() {
         posts.map(post => (
           <div key={post.attributes.publishedAt} className="card p-2 m-2">
             <Link className="text-decoration-none text-dark" href={`/posts/${post.id}`}>
-            <p>{post.attributes.id}</p>
-            <h5 className="card-title text-center">{post.attributes.title}</h5>
-            <h6 className="card-text">{post.attributes.description}</h6>
+
+              <div className="d-flex align-items-center justify-content-between ">
+                <Image className="card-image img-thumbnail" src={`http://localhost:1337${post.attributes.Image.data.attributes.formats.thumbnail.url}`}
+                  // height={post.attributes.Image.data.attributes.formats.thumbnail.height}
+                  // width={post.attributes.Image.data.attributes.formats.thumbnail.width}
+                  height={150}
+                  width={150}
+                />
+                <div className="">
+                  <h5 className="card-title text-center">{post.attributes.title}</h5>
+                  <h6 className="card-text text-center p-2">{post.attributes.description}</h6>
+                </div>
+
+              </div>
             </Link>
           </div>
         ))
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "**"
+            }
+        ]
+      },
+
+};
 
 export default nextConfig;
