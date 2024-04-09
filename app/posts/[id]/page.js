077777c0d8e34d3038b0acddaab3ecf0eae0e22c
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2024-04-09 08:37:33 -0400

    update paragraphs in select blog page view

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -14,7 +14,17 @@ async function PostPage(props) {
   //console.log(posts)
   const post = posts.filter((post) => post.id == id)
   const postParagraphs = post[0].attributes.content.map(el => el.children).map(el => el[0]).map(el => el.text)
-  console.log(post)
+  //console.log(post[0].attributes.content[0].children[0].text)
+  console.log(post[0].attributes.content[1].children[0].text)
+
+  const paragraphOne = post[0].attributes.content[0].children[0].text;
+  const paragraphTwo = post[0].attributes.content[1].children[0].text;
+  const paragraphThree = post[0].attributes.content[2].children[0].text
+  const paragraphFour = post[0].attributes.content[3].children[0].text;
+  const paragraphFive = post[0].attributes.content[4].children[0].text;
+  const paragraphSix = post[0].attributes.content[5] ? post[0].attributes.content[5].children[0].text : "";
+
+
   // const postImageUrl = `http://localhost:1337${post[0].attributes.Image.data.attributes.url}`
   // const imageWidth = post[0].attributes.Image.data.attributes.width
   // const imageHeigt = post[0].attributes.Image.data.attributes.height
@@ -35,7 +45,14 @@ async function PostPage(props) {
           className="m-auto img-fluid card-image"
         /> */}
         {/* <h6 className="card-text lead p-4">{post[0].attributes.content[0].children[0].text}</h6> */}
-        <h6 className="card-text lead p-4">{postParagraphs}</h6>
+        {/* <h6 className="card-text lead p-4">{postParagraphs}</h6> */}
+        <article>
+        <h6 className="card-text lead p-1 m-1">{paragraphOne}</h6>
+        <h6 className="card-text lead p-1 m-1">{paragraphTwo}</h6>
+        <h6 className="card-text lead p-1 m-1">{paragraphThree}</h6>
+        <h6 className="card-text lead p-1 m-1">{paragraphFour}</h6>
+        <h6 className="card-text lead p-1 m-1">{paragraphFive}</h6>
+        </article>
       </div>
       <Footer />
     </div>
